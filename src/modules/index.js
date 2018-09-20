73195afe43a9ce9a42d937e6535676f4cb0631ac
Tynan Ford <fordtyna@msu.edu>
2018-09-20 04:32:25 -0400

    Site looking good

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,4 +1,10 @@
 import _ from 'lodash';
 require('./base.js');
+require('./bulma-accordion.js');
+import bulmaAccordion from './bulma-accordion.js'
 require('../yellowstone.jpg');
 
+
+$(document).ready(function() { 
+  var accordions = bulmaAccordion.attach();
+});
